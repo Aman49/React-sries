66049b53f689ddd20ccd61c9eb3d7ca2fe2e632f
Aman kumar <amanshrivast49@gmail.com>
2023-11-15 07:28:16 +0530

    first

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello React js | it is an frontend Libraray which is used to make single page application.</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Hello react | Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects.</h1>
+  )
+}
+
+export default App
